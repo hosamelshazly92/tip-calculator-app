57e470f0ce19c8f9762d9f80a6dfa9582782803a
hosamelshazly92 <hosamelshazly92@gmail.com>
2021-09-19 01:12:38 +0200

    create responsive design

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { container, top, bottom } from "./App.module.css";
+import { container, top, bottom, image } from "./App.module.css";
 import Calculator from "./components/Calculator/Calculator";
 
 function App() {
@@ -6,6 +6,7 @@ function App() {
     <div className={container}>
       <div className={top}>
         <img
+          className={image}
           src="https://res.cloudinary.com/ux-ui-designer/image/upload/v1631973900/Assets/logo_j8qz2i.svg"
           alt="main logo"
         />
