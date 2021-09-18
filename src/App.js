6799d7867f56ffabc114a555969248068460f875
hosamelshazly92 <hosamelshazly92@gmail.com>
2021-09-18 16:48:21 +0200

    create Calculator component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
-import {} from "./App.module.css";
+import { container, top, bottom } from "./App.module.css";
+import Calculator from "./components/Calculator/Calculator";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className={container}>
+      <div className={top}>
+        <img
+          src="https://res.cloudinary.com/ux-ui-designer/image/upload/v1631973900/Assets/logo_j8qz2i.svg"
+          alt="main logo"
+        />
+      </div>
+      <div className={bottom}>
+        <Calculator />
+      </div>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Calculator/Calculator.js
@@ -0,0 +1,18 @@
+import { container, left, right } from "./Calculator.module.css";
+
+const Calculator = () => {
+  return (
+    <div className={container}>
+      <div className={left}>
+        <h4>Bill</h4>
+        <h4>Bill</h4>
+      </div>
+      <div className={right}>
+        <h4>Bill</h4>
+        <h4>Bill</h4>
+      </div>
+    </div>
+  );
+};
+
+export default Calculator;
